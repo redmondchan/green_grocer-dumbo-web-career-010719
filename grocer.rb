9cc5ad06e5b046b35692a9d0d59af1c4b4724802
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 03:39:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |hash|
+    hash.each do |name, describe|
+      if new_cart |name|
+        new_cart[name][:count] += 1
+      else
+        new_cart[name] = describe
+        new_cart[name][:count] = 1
+      end
+    end
+  end
+  new_cart# code here
 end
 
 def apply_coupons(cart, coupons)
